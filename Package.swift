c1574162b3429dbb7e1d979dfe8ad7caff5f2026
Tom Holland <codesman@hackattic.net>
2017-01-03 19:05:52 -0800

    SPM test

--- Package.swift
+++ Package.swift
@@ -2,6 +2,11 @@ import PackageDescription
 
 let package = Package(
     name: "Fakery",
+	// targets: [
+    //     Target(name: "Config"),
+	// 	Target(name: "Data", dependencies: ["Config"]),
+	// 	// Target(name: "Parser", dependencies: ["Config"]),
+    // ],
 	exclude: [
 		"Resources",
 		"Project",
